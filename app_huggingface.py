a8da66bb496a39e728b7d9c5344396c7796ad62c
Azie88 <obandoandrew8@gmail.com>
2024-06-30 17:36:12 +0300

    huggingfacehub

--- app_huggingface.py
+++ app_huggingface.py
@@ -4,6 +4,11 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import history_aware_retriever
+from langchain.llms import huggingface_hub
+from htmlfiles import css, user_template, bot_template
+
 
 
 def get_pdf_text(pdf_docs):
@@ -34,6 +39,34 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 
+def get_conversation_chain(vectorstore):
+    llm = huggingface_hub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    memory = ConversationBufferMemory(
+        memory_key='chat_history',
+        return_message=True
+    )
+    conversation_chain = history_aware_retriever(
+        llm = llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+
+def  handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content),unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content),unsafe_allow_html=True)
+
+
+
+
 
 def main():
 
@@ -41,8 +74,24 @@ def main():
 
     st.set_page_config(page_title='PDF Chatbot', page_icon='🤖')
 
+
+    st.write(css,unsafe_allow_html=True)
+
+
+
+    if 'conversation' not in st.session_state:
+        st.session_state.conversation = None
+
+    if 'chat_history' not in st.session_state:
+        st.session_state.chat_history = None
+
+
     st.header('Chat with your PDF file!! 📄')
-    st.text_input('What do you want to know from your document?')
+    user_question = st.text_input('What do you want to know from your document?')
+
+    if user_question:
+        handle_userinput(user_question)
+
 
     with st.sidebar:
         st.subheader('Your files')
@@ -60,6 +109,9 @@ def main():
 
                 #create vector store
                 vectorstore = get_vectorstore(text_chunks)
+
+                #coversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
 
 if __name__ == '__main__':
