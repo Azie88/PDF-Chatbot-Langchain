ee39d35afca3ffbda31a61e663c78a701577dd7a
Azie88 <obandoandrew8@gmail.com>
2024-07-04 16:20:20 +0300

    Open AI

--- app_openai.py
+++ app_openai.py
@@ -2,11 +2,14 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_community.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import history_aware_retriever
+from langchain.chains import ConversationalRetrievalChain
 from htmlfiles import css, user_template, bot_template
+
+
 
 
 
@@ -17,6 +20,7 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
 
 
 def get_text_chunks(text):
@@ -30,6 +34,7 @@ def get_text_chunks(text):
     return chunks
 
 
+
 def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings()
     vectorstore = FAISS.from_texts(
@@ -38,13 +43,14 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 
+
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
     memory = ConversationBufferMemory(
         memory_key='chat_history',
-        return_message=True
+        return_messages=True
     )
-    conversation_chain = history_aware_retriever(
+    conversation_chain = ConversationalRetrievalChain.from_llm(
         llm = llm,
         retriever=vectorstore.as_retriever(),
         memory=memory
@@ -52,46 +58,49 @@ def get_conversation_chain(vectorstore):
     return conversation_chain
 
 
-def  handle_userinput(user_question):
+
+def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
+    #st.write(response)
     st.session_state.chat_history = response['chat_history']
 
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-            st.write(user_template.replace("{{MSG}}", message.content),unsafe_allow_html=True)
+            st.write(user_template.replace(
+                "{{MSG}}", message.content),unsafe_allow_html=True)
         else:
-            st.write(bot_template.replace("{{MSG}}", message.content),unsafe_allow_html=True)
-
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content),unsafe_allow_html=True)
 
 
 
 def main():
-
+    # load env
     load_dotenv()
 
     st.set_page_config(page_title='PDF Chatbot', page_icon='🤖')
-
-
     st.write(css,unsafe_allow_html=True)
-
-
+    
+    
     if 'conversation' not in st.session_state:
         st.session_state.conversation = None
-
     if 'chat_history' not in st.session_state:
         st.session_state.chat_history = None
-
-
+    
     st.header('Chat with your PDF file!! 📄')
     user_question = st.text_input('What do you want to know from your document?')
-
     if user_question:
         handle_userinput(user_question)
+    #st.write(user_template.replace('{{MSG}}', 'Hello Robot'),unsafe_allow_html=True)
+    #st.write(bot_template.replace('{{MSG}}', 'Hello Human'),unsafe_allow_html=True)
+
+
 
 
     with st.sidebar:
         st.subheader('Your files')
-        pdf_docs = st.file_uploader('Upload your PDF files here', accept_multiple_files=True)
+        pdf_docs = st.file_uploader(
+            'Upload your PDF files here', accept_multiple_files=True)
         if st.button('Load'):
             with st.spinner('Loading'):
 
@@ -108,6 +117,10 @@ def main():
 
                 #coversation chain
                 st.session_state.conversation = get_conversation_chain(vectorstore)
+            
+
+
+
 
 
 if __name__ == '__main__':
